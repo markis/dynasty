db4cc835fc869eb5ffe0b46aa3b943a5e0fd0b02
Markis Taylor <m@rkis.net>
2024-06-29 23:00:11 -0400

    fix: make back_fill not default

--- dynasty/__main__.py
+++ dynasty/__main__.py
@@ -1,12 +1,12 @@
 import logging
 import os
-from collections.abc import Generator
+from collections.abc import Container, Generator
 from uuid import UUID
 
 from tqdm import tqdm
 
 from dynasty.db import Session, create_database, upsert_player_rankings, upsert_players
-from dynasty.models import LeagueType, PlayerRanking
+from dynasty.models import LeagueType, PlayerRanking, RankingSet
 from dynasty.service.dynasty_process import DynastyProcess
 from dynasty.service.keeptradecut import KTCService
 from dynasty.service.sleeper import SleeperService
@@ -21,33 +21,37 @@ class PlayerRankingGenerator:
     def __init__(self) -> None:
         self.player_ids = set()
 
-    def __call__(self, *, back_fill: bool) -> Generator[PlayerRanking, None, None]:
+    def __call__(
+        self, *, back_fill: bool, ranking_sets: Container[RankingSet] | None = None
+    ) -> Generator[PlayerRanking, None, None]:
         ranking_count = 0
         player_ids: set[UUID] = set()
 
-        with KTCService() as ktc_service:
-            for league_type in (LeagueType.Standard, LeagueType.SuperFlex):
-                rankings = (
-                    ktc_service.get_player_full_history(league_type)
-                    if back_fill
-                    else ktc_service.get_rankings(league_type)
-                )
-                for ranking in tqdm(rankings, desc=f"Mapping KTC players ({league_type})"):
+        if not ranking_sets or RankingSet.KeepTradeCut in ranking_sets:
+            with KTCService() as ktc_service:
+                for league_type in (LeagueType.Standard, LeagueType.SuperFlex):
+                    rankings = (
+                        ktc_service.get_player_full_history(league_type)
+                        if back_fill
+                        else ktc_service.get_rankings(league_type)
+                    )
+                    for ranking in tqdm(rankings, desc=f"Mapping KTC players ({league_type})"):
+                        player_ids.add(ranking.player_id)
+                        yield ranking
+                        ranking_count += 1
+
+        if not ranking_sets or RankingSet.DynastyProcess in ranking_sets:
+            with DynastyProcess() as dp_service:
+                rankings = dp_service.get_rankings(back_fill=back_fill)
+                for ranking in tqdm(rankings, desc="Mapping Dynasty Process players"):
                     player_ids.add(ranking.player_id)
                     yield ranking
                     ranking_count += 1
 
-        with DynastyProcess() as dp_service:
-            rankings = dp_service.get_rankings(back_fill=back_fill)
-            for ranking in tqdm(rankings, desc="Mapping Dynasty Process players"):
-                player_ids.add(ranking.player_id)
-                yield ranking
-                ranking_count += 1
-
         logger.info("Ranking Count: %d", ranking_count)
 
 
-def import_players(*, back_fill: bool = False) -> None:
+def import_players(*, back_fill: bool = False, ranking_sets: Container[RankingSet] | None = None) -> None:
     """
     Import player rankings from KeepTradeCut and Sleeper into the database.
     """
@@ -56,7 +60,7 @@ def import_players(*, back_fill: bool = False) -> None:
 
     rankings = PlayerRankingGenerator()
     with Session(engine) as session:
-        player_rankings = rankings(back_fill=back_fill)
+        player_rankings = rankings(back_fill=back_fill, ranking_sets=ranking_sets)
         upsert_player_rankings(session, player_rankings)
         session.commit()
 
